af36584887b06f9c0229ab04201804167847435b
Habib Kazemi <hbkazemi96@gmail.com>
2025-03-21 20:59:00 +0100

    Initial changes to make it habib's personal page

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+export const projects = [
+    {
+        title: "Project 1",
+        description: "Description of project 1",
+        link: "https://github.com/yourusername/project1",
+        tags: ["TypeScript", "React", "Node.js"],
+        year: "2024",
+    },
+    {
+        title: "Project 2",
+        description: "Description of project 2",
+        link: "https://github.com/yourusername/project2",
+        tags: ["Python", "Machine Learning"],
+        year: "2023",
+    },
+];
